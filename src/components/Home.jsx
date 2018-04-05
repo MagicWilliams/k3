60ff03e135e3f3c289aa2f1842d56e294fe70791
David Latimore <dgl@u.northwestern.edu>
2018-04-05 18:26:24 -0500

    ui layout done, need content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,19 +10,34 @@ class Home extends React.Component {
     }
 
     render() {
+
         return ( 
-            <div className="Home">
-                <div className="left-col">
-                    <h1 id="name"> david latimore ii </h1>
-                    <div id="link-section"> 
-                        <a href="https://www.github.com/MagicWilliams"> code </a>
-                        <a href="https://www.twitter.com/MagicWilliams"> tweets </a>
-                        <a href=""> resume </a>
-                        <a href="https://www.dialupstuff.com"> dialupstuff </a>
+            <div> 
+                <div className="Home-body">
+                    <div className="left-col">
+                        <h1 id="name"> david latimore ii </h1>
+                        <div id="link-section"> 
+                            <a href="https://www.github.com/MagicWilliams">code</a>
+                            <a href="https://www.twitter.com/MagicWilliams">tweets</a>
+                            <a href="">resume</a>
+                            <a href="https://www.dialupstuff.com">dialupstuff</a>
+                        </div>
+                    </div>
+                    <div className="right-col">
+                        <div id="project-view">
+                        </div> 
+                        <h5 id="project-caption"> placholding caption </h5>
                     </div>
                 </div>
-                <div className="right-col">
-                    <div id="project-view">
+                <div className="Home-footer">
+                    <h4> stuff i've made recently </h4>
+                    <div className="project-section">
+                        <ul>
+                            <li>a design spec for social works chicago</li>
+                            <li>an album, available at dialupstuff.com</li>
+                            <li>a site for Psuedo Knitting Club</li>
+                            <li>a PSN account, play fortnite w me</li>
+                        </ul>
                     </div> 
                 </div>
             </div>
