c9913ab09a09c8ebdff25f501d62979d900bab52
David Latimore <dgl@u.northwestern.edu>
2018-04-17 16:07:36 -0700

    rewrote the layout css now that i actually kinda know how to usee grid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,32 +13,31 @@ class Home extends React.Component {
 
         return ( 
             <div> 
-                <div className="Home-body">
-                    <div className="left-col">
-                        <h1 id="name"> david latimore ii </h1>
-                        <div id="link-section"> 
-                            <a target="_blank" href="https://www.github.com/MagicWilliams">code</a>
-                            <a target="_blank" href="https://www.twitter.com/MagicWilliams">tweets</a>
-                            <a target="_blank" href="">resume</a>
-                            <a target="_blank" href="http://dialupstuff.com">dialupstuff</a>
+                <div className="grid">
+                    <div className="item">
+                        <div className="item1-content">
+                            <h1 id="name"> testing for name </h1>
+                            <div id="link-section"> 
+                                <a target="_blank" href="https://www.github.com/MagicWilliams">code</a>
+                                <a target="_blank" href="https://www.twitter.com/MagicWilliams">tweets</a>
+                                <a target="_blank" href="">resume</a>
+                                <a target="_blank" href="http://dialupstuff.com">dialupstuff</a>
+                            </div>
                         </div>
                     </div>
-                    <div className="right-col">
-                        <div id="project-view">
-                        </div> 
-                        <h5 id="project-caption"> placholding caption </h5>
+                    <div className="item2">
+                        <div className="item2-content">
+                            <div id="project-view">
+                            </div> 
+                            <h5 id="project-caption"> placholding caption </h5>
+                        </div>
                     </div>
-                </div>
-                <div className="Home-footer">
-                    <h4> stuff i've made recently </h4>
-                    <div className="project-section">
-                        <ul>
-                            <li>a design spec for social works chicago</li>
-                            <li>an album, available at dialupstuff.com</li>
-                            <li>a site for Psuedo Knitting Club</li>
-                            <li>a PSN account, play fortnite w me</li>
-                        </ul>
-                    </div> 
+                    <div className="item3">
+                        <h4> stuff i've made recently </h4>
+                        <div className="project-section">
+                            <p className = "marquee"><span> a design spec for social works chicago              an album, available at dialupstuff.com           a site for Psuedo Knitting Club               a PSN account, play fortnite w me             </span></p>
+                        </div> 
+                    </div>
                 </div>
             </div>
         );
