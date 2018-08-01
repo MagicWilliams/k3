9d73bdeba91b07098aa88e4365d698e30a3c0964
David Latimore <dgl@u.northwestern.edu>
2018-08-01 13:11:13 -0700

    clean slate smfh i'm never doing this again in this repo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,47 +6,15 @@ class Home extends React.Component {
 	constructor(props) {
         super(props);
         this.state = {
-            desc: [
-                "a UI i designed for SocialWorks Chicago", 
-                "an EP i made with @dialupstuff. Available on Apple Music and Spotify.",
-                "a site for rogue agency, a music label in new york"
-            ],
-
-            mediaLinks: [
-                "    ", 
-                "    ",
-                "    "
-            ],
 
         }
     }
 
     render() {
 
-        return ( 
-            <div> 
-                <div className="grid">
-                    <div className="item">
-                        <div className="item1-content">
-                            <h1 id="name"> david latimore ii </h1>
-                            <div id="link-section"> 
-                                <a target="_blank" href="https://www.github.com/MagicWilliams">code</a>
-                                <a target="_blank" href="https://www.twitter.com/MagicWilliams">tweets</a>
-                                <a target="_blank" href="">resume</a>
-                                <a target="_blank" href="http://dialupstuff.com">dialupstuff</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item2">
-                        <div className="item2-content">
-                            <ProjectView />
-                            {/* <h5 id="project-caption"> placholding caption </h5> */}
-                        </div>
-                    </div>
-                    <div className="item3">
-
-                    </div>
-                </div>
+        return (
+            <div>
+                lmaoooo
             </div>
         );
     }
